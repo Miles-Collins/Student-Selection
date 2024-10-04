c276616a114073a3d1a652b829324e22d7bafb57
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2024-10-03 17:25:58 -0700

    Added Student class/updated app

--- student_selection/src/main/java/com/main/App.java
+++ student_selection/src/main/java/com/main/App.java
@@ -8,24 +8,23 @@ public class App {
         System.out.println("Enter the number of students: ");
         try (Scanner scanner = new Scanner(System.in)) {
             int numberOfStudents = scanner.nextInt();
-            String[] students = new String[numberOfStudents];
-            int[] age = new int[numberOfStudents];
-            Scanner scanNames = new Scanner(System.in);
-            Scanner scanAge = new Scanner(System.in);
+            Student[] students = new Student[numberOfStudents];
+            Scanner scanDetails = new Scanner(System.in);
             for (int i = 0; i < numberOfStudents; i++) {
                 System.out.println("Enter the name of student " + (i + 1) + ": ");
-                students[i] = scanNames.nextLine();
-                System.out.println("Enter the age of student " + (i + 1) + ": ");
-                age[i] = scanAge.nextInt();
+                String name = scanDetails.nextLine();
+                System.out.println("Enter the age of " + (i + 1) + ": ");
+                int age = scanDetails.nextInt();
+                scanDetails.nextLine(); // Consume newline
+                students[i] = new Student(name, age);
             }
             for (int i = 0; i < numberOfStudents; i++) {
-                if (age[i] > 15) {
-                    System.out.println(students[i] + " is old enough to receive a vaccine.");
+                if (students[i].getAge() > 15) {
+                    System.out.println(students[i].getName() + " is old enough to receive a vaccine.");
                 } else {
-                    System.out.println(students[i] + " is not old enough to receive a vaccine.");
+                    System.out.println(students[i].getName() + " is not old enough to receive a vaccine.");
                 }
             }
-
         }
     }
 }
